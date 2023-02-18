e2b210ad19b5268f42f3f3a64d44cd2b1affa9f4
Abhijit Gupta <avgupta456@gmail.com>
2023-02-18 14:04:33 -0500

    Better prompt

--- frontend/src/pages/api/openai.tsx
+++ frontend/src/pages/api/openai.tsx
@@ -12,13 +12,25 @@ export default async function handler(req, res) {
 
   let prompt = `Create the simplest valid Postgres SQL query to answer the question. Infer specific rows wanted, and limit to 20 rows if not specified.
 
+  START
   Schema:
-  Table customers, columns = [CustomerId, FirstName, LastName, Company, Address, City, State, Country, PostalCode, Phone, Fax, Email, SupportRepId]
+  Table customers, columns = [customerId, firstName, lastName, company, address, city, state, country, postalCode, phone, fax, email, supportRepId]
   
-  Question: Create a MySQL query for all customers in Texas named Jane
-  
-  Answer: SELECT customerId, firstName, lastName, State FROM customers WHERE State = 'Texas' AND FirstName = 'Jane'
-  
+  Question: List all customers in Texas named Jane
+
+  Answer: SELECT customerId, firstName, lastName, state FROM customers WHERE state = 'Texas' AND firstName = 'Jane'
+  END
+
+  START
+  Schema:
+  Table sp500, columns = [market_cap, year_high, ebidta, price, price_per_earning, earning_per_share, year_low, sector, symbol, name]
+
+  Question: What is the price-per-earning grouped by sector?
+
+  Answer: SELECT sector, AVG(price_per_earning) FROM sp GROUP BY sector
+  END
+
+  START
   Schema:
 `;
 
@@ -35,11 +47,11 @@ export default async function handler(req, res) {
     model: "code-davinci-002",
     prompt: prompt,
     temperature: 0,
-    max_tokens: 256,
+    max_tokens: 64,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
-    stop: ["Schema:", "Question:", "Answer:"],
+    stop: ["END", "Schema:", "Question:", "Answer:"],
   });
 
   return res.status(200).json({ response: response.data });
